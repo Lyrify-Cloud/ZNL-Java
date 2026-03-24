111f5c023d45fa654c5f5a49f01670ff205d24fc
kukemc <2464952636@qq.com>
2026-03-25 00:45:05 +0800

    Add znl-java implementation, tests, and documentation

--- src/main/java/com/znl/TestCrypto.java
+++
@@ -1,9 +0,0 @@
-package com.znl;
-
-public class TestCrypto {
-    public static void main(String[] args) throws Exception {
-        SecurityUtils.Keys keys = SecurityUtils.deriveKeys("my-secret");
-        System.out.println("signKey hex: " + SecurityUtils.bytesToHex(keys.signKey));
-        System.out.println("encryptKey hex: " + SecurityUtils.bytesToHex(keys.encryptKey));
-    }
-}
--- src/main/java/com/znl/ZmqRequestHandler.java
+++
@@ -1,8 +0,0 @@
-package com.znl;
-
-import java.util.concurrent.CompletableFuture;
-
-@FunctionalInterface
-public interface ZmqRequestHandler {
-    CompletableFuture<byte[]> handle(ZmqEvent event) throws Exception;
-}
